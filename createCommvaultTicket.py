13b17998621dc809bee153707b93a11dc66a6a4a
Charles <c1mckay@sdsc.edu>
2017-04-21 11:59:21 -0700

    fixed bug where main wouldn't run automatically

--- createCommvaultTicket.py
+++ createCommvaultTicket.py
@@ -3,20 +3,17 @@ import json, subprocess, random, sys
 import credentials
 from keystoneclient.v3 import client
 
-from pyfootprints.footprintsEditor import createTicket, editTicket
+from pyfootprints.footprintsEditor import createTicket
 from db_communicator import query
 
 def main(project_name, shouldPrint = True):
-	if len(sys.argv) < 2:
-		return
 	ticketNumber = createTicket(project_name = project_name, billing_index = 'CACNULL', first_name = 'Andrew', last_name = 'Ferbert', email = 'dferbert@sdsc.edu', title='Commvault Invoice - {0}', t_assignees=['dferbert'])
 	query("insert into commvault_tickets (name, ticket_number) VALUES (%s, %s);", [project_name, ticketNumber], False)
 	if shouldPrint:
 		print ticketNumber
 	return ticketNumber
 
-
-if __name__ == "__main__":
+if __name__ == "__main__" and len(sys.argv) == 2:
 	project_name = sys.argv[1]
 	main(project_name = project_name)
 
